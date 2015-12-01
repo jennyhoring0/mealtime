27e182905a02288e55a54d507df3b4385df01272
jennyhoring <jehoring@gmail.com>
2015-11-30 21:38:34 -0500

    Initial Commit

---
+++ MealTime/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MealTime
+//
+//  Created by Jennifer Horing on 11/30/15.
+//  Copyright (c) 2015 Jennifer Horing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MealTime/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  MealTime
+//
+//  Created by Jennifer Horing on 11/30/15.
+//  Copyright (c) 2015 Jennifer Horing. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
